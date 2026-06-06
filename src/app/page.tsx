01c0807aa799b5dc3be8f5c5728ee736c5d1dd71
iamdityacoder <iamadityacoder01@email.com>
2026-06-06 18:41:31 +0530

    rearrange layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
         <Hero />
         <TrustSection />
         <AboutSection />
+        <GallerySection />
         <CoursesSection />
         <BenefitsSection />
         <ReviewsSection />
-        <GallerySection />
         <ContactSection />
       </main>
       <Footer />
